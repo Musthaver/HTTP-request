2345fd7d9753a1a331f18ae1e13a25f0c599872b
Robin Westhaver <robinwesthaver@gmail.com>
2019-05-06 18:27:55 +0000

    added print_1337

--- callbacks/print_1337.js
+++ callbacks/print_1337.js
@@ -4,3 +4,32 @@ var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step6/1337.html'
 };
+
+var obj1337 = {
+    a: '4',
+    e: '3',
+    l: '1',
+    o: '0',
+    s: '5',
+    t: '7',
+    'ck': 'x',
+    'er': '0r',
+    'you': 'j00'
+}
+
+function print1337 (html) {
+
+var finalText = "";
+
+for (i = 0; i < html.length; i++) {
+    if (obj1337.hasOwnProperty(html[i])) {
+        finalText += obj1337[html[i]];
+    } else {
+        finalText += html[i];
+    }
+}
+console.log(finalText);
+  
+}
+  
+getHTML(requestOptions, print1337);
