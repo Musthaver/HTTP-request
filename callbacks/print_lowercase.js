56714224623fe4c21ec252f6fc1564109d6e42ea
Robin Westhaver <robinwesthaver@gmail.com>
2019-05-06 18:27:25 +0000

    added print_lowercase

--- callbacks/print_lowercase.js
+++ callbacks/print_lowercase.js
@@ -4,3 +4,10 @@ var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step6/lowercase.html'
 };
+
+function printLowerCase (html) {
+    var lowerCase = html.toLowerCase();
+    console.log(lowerCase);
+}
+  
+getHTML(requestOptions, printLowerCase);
