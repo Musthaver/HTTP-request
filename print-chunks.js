d4deaae5fe512a774328bc6cfbd33a445f88119c
Robin Westhaver <robinwesthaver@gmail.com>
2019-05-06 16:20:12 +0000

    Successfully got chunks to log as they come in

---
+++ print-chunks.js
@@ -0,0 +1,27 @@
+// require https module
+var https = require('https');                               
+
+
+function getAndPrintHTMLChunks () {
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step1.html'
+    };
+  
+ // call the GET method of HTTPS module to get the response
+    https.get(requestOptions, function (response) {
+
+    // set encoding of received data to UTF-8
+        response.setEncoding('utf8');
+    
+        // the callback is invoked when a `data` chunk is received
+        response.on('data', function (data) {
+        console.log(data + "\n");
+        });
+    
+    });
+    
+}
+
+getAndPrintHTMLChunks();
